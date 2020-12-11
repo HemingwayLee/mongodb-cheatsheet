472f178bb9344698c08ded118282b4aafcc78532
LeeHemingway <coherence0815@gmail.com>
2020-12-11 21:17:08 +0900

    update index examples

--- cloud/dynamodb/localServer/aws-cli/index/query_with_sli_index.sh
+++ cloud/dynamodb/localServer/aws-cli/index/query_with_sli_index.sh
@@ -3,32 +3,35 @@
 export AWS_ACCESS_KEY_ID='DUMMYIDEXAMPLE'
 export AWS_SECRET_ACCESS_KEY='DUMMYEXAMPLEKEY'
 
+# It must have a RANGE key in table
+# It must have a RANGE key in index as well
 aws dynamodb create-table \
   --table-name Students \
   --attribute-definitions \
     AttributeName=MyName,AttributeType=S \
     AttributeName=Age,AttributeType=N \
+    AttributeName=Title,AttributeType=S \
   --key-schema \
     AttributeName=MyName,KeyType=HASH \
+    AttributeName=Age,KeyType=RANGE \
   --region us-west-2 \
   --endpoint-url http://localhost:8000 \
   --provisioned-throughput \
     ReadCapacityUnits=10,WriteCapacityUnits=5 \
-  --global-secondary-indexes \
+  --local-secondary-indexes \
       '[
           {
-              "IndexName": "AgeIndex",
+              "IndexName": "TitleIndex",
               "KeySchema": [{
-                "AttributeName":"Age",
+                "AttributeName":"MyName",
                 "KeyType":"HASH"
+              }, {
+                "AttributeName":"Title",
+                "KeyType":"RANGE"
               }],
               "Projection":{
                 "ProjectionType":"INCLUDE",
-                "NonKeyAttributes":["Title"]
-              },
-              "ProvisionedThroughput": {
-                "ReadCapacityUnits": 10,
-                "WriteCapacityUnits": 5
+                "NonKeyAttributes":["Location"]
               }
           }
       ]'
@@ -42,19 +45,19 @@ aws dynamodb batch-write-item --region us-west-2 \
 # See all data
 # aws dynamodb scan --table-name Students --region us-west-2 --endpoint-url http://localhost:8000
 
-# The item which has title
+# primary key need to be specified, not very useful
 aws dynamodb query --table-name Students \
-  --index-name AgeIndex \
-  --key-condition-expression "Age = :aaa" \
-  --expression-attribute-values  '{":aaa":{"N":"20"}}' \
+  --index-name TitleIndex \
+  --key-condition-expression "Title = :ttt and MyName = :mn" \
+  --expression-attribute-values  '{":ttt":{"S":"Student"},":mn":{"S":"Chris"}}' \
   --region us-west-2 \
   --endpoint-url http://localhost:8000
 
-# The item which does not have title
+# You can not search by sort key only, primary key need to be specified
 aws dynamodb query --table-name Students \
-  --index-name AgeIndex \
-  --key-condition-expression "Age = :aaa" \
-  --expression-attribute-values  '{":aaa":{"N":"25"}}' \
+  --index-name TitleIndex \
+  --key-condition-expression "Title = :ttt" \
+  --expression-attribute-values  '{":ttt":{"S":"Techlead"}}' \
   --region us-west-2 \
   --endpoint-url http://localhost:8000
 
