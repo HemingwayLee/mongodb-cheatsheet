c8970cfbcd09bca12479a1f73f53b62111a862fc
LeeHemingway <coherence0815@gmail.com>
2020-12-11 09:00:58 +0900

    add another insertion example

--- cloud/dynamodb/localServer/aws-cli/README.md
+++ cloud/dynamodb/localServer/aws-cli/README.md
@@ -1,4 +1,4 @@
-# Sample code
+# Sample code explanation
 
 ```
 #!/bin/bash
@@ -11,5 +11,10 @@ aws dynamodb list-tables --endpoint-url http://localhost:8000 --region us-west-2
 
 * `AWS_ACCESS_KEY_ID` and `AWS_SECRET_ACCESS_KEY` can be anything, and they must be set
 * `--endpoint-url` and `--region` must be in the command
+
+# Note
+* dynamodb is schemaless (except key schema)
+* The number of `--attribute-definitions` and the number of `--key-schema` must match
+
 
 
---
+++ cloud/dynamodb/localServer/aws-cli/insert_data2.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+export AWS_ACCESS_KEY_ID='DUMMYIDEXAMPLE'
+export AWS_SECRET_ACCESS_KEY='DUMMYEXAMPLEKEY'
+
+# The number of --attribute-definitions and the number of --key-schema must match
+aws dynamodb create-table \
+  --table-name Students \
+  --attribute-definitions \
+    AttributeName=Name,AttributeType=S \
+  --key-schema \
+    AttributeName=Name,KeyType=HASH \
+  --region us-west-2 \
+  --endpoint-url http://localhost:8000 \
+  --provisioned-throughput \
+    ReadCapacityUnits=10,WriteCapacityUnits=5
+
+aws dynamodb list-tables --endpoint-url http://localhost:8000 --region us-west-2
+
+aws dynamodb put-item --table-name Students --region us-west-2 \
+  --endpoint-url http://localhost:8000 \
+  --item '{"Name": {"S": "James"}, "Age": {"N": "26"}}'
+
+aws dynamodb put-item --table-name Students --region us-west-2 \
+  --endpoint-url http://localhost:8000 \
+  --item '{"Name": {"S": "Kenny"}, "Company": {"S": "TrendMicro"} }'
+
+aws dynamodb scan --table-name Students --region us-west-2 \
+  --endpoint-url http://localhost:8000
+
+
