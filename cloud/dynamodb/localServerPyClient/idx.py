5107ee2a28befb39797a57f3d60dec609279be44
LeeHemingway <coherence0815@gmail.com>
2020-12-19 15:01:08 +0900

    update python examples

--- cloud/dynamodb/localServerPyClient/idx.py
+++ cloud/dynamodb/localServerPyClient/idx.py
@@ -1,51 +1,89 @@
 import os
 import boto3
+from boto3.dynamodb.conditions import Key
 
-# According to document, `endpoint_url` and `region_name` were not there
-# it has to be the argument for resource()
 dynamodb = boto3.resource(
     'dynamodb', 
     endpoint_url="http://localhost:8000",
     region_name='us-west-2')
 
-table = dynamodb.create_table(
-    TableName='users',
-    KeySchema=[
-        {
-            'AttributeName': 'username',
-            'KeyType': 'HASH'
+def create_table():
+    table = dynamodb.create_table(
+        TableName='users',
+        KeySchema=[
+            {
+                'AttributeName': 'username',
+                'KeyType': 'HASH'
+            },
+            {
+                'AttributeName': 'last_name',
+                'KeyType': 'RANGE'
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'username',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'last_name',
+                'AttributeType': 'S'
+            },
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 4000,
+            'WriteCapacityUnits': 4000
         },
-        {
-            'AttributeName': 'last_name',
-            'KeyType': 'RANGE'
+        GlobalSecondaryIndexes=[
+            {
+                'IndexName': 'idxLastName',
+                'KeySchema': [
+                    {
+                        'AttributeName': 'last_name',
+                        'KeyType': 'HASH'
+                    },
+                ],
+                'Projection': {
+                    'ProjectionType': 'INCLUDE',
+                    'NonKeyAttributes': [
+                        'AAA',
+                    ]
+                },
+                'ProvisionedThroughput': {
+                    'ReadCapacityUnits': 4000,
+                    'WriteCapacityUnits': 4000
+                }
+            },
+        ]
+    )
+
+    table.meta.client.get_waiter('table_exists').wait(TableName='users')
+    print(table.item_count)
+    return table
+
+def put_item(table):
+    response = table.put_item(
+        Item={
+            'username': 'ywlee',
+            'last_name': 'YunWei',
+            'AAA': 'BBB'
         }
-    ],
-    AttributeDefinitions=[
-        {
-            'AttributeName': 'username',
-            'AttributeType': 'S'
-        },
-        {
-            'AttributeName': 'last_name',
-            'AttributeType': 'S'
-        },
-    ],
-    ProvisionedThroughput={
-        'ReadCapacityUnits': 4000,
-        'WriteCapacityUnits': 4000
-    }
-)
-
-table.meta.client.get_waiter('table_exists').wait(TableName='users')
-print(table.item_count)
-
-response = table.put_item(
-    Item={
-        'username': 'ywlee',
-        'last_name': 'YunWei'
-    }
-)
-print(table.item_count)
+    )
+    print(table.item_count)
 
 
+def query(table):
+    response = table.query(
+        IndexName='idxLastName',
+        KeyConditionExpression=Key('last_name').eq('YunWei')
+    )
+        
+    items = response['Items']
+    for item in items:
+        print(item['last_name'], ":", item['AAA'])
 
+
+if __name__ == '__main__':
+    table = create_table()
+    put_item(table)
+    query(table)
---
+++ cloud/dynamodb/localServerPyClient/timestamp.py
@@ -0,0 +1,150 @@
+import os
+import boto3
+import datetime
+import math
+import time
+from random import randint
+from boto3.dynamodb.conditions import Key
+from datetime import timezone, timedelta, date
+
+dynamodb = boto3.resource(
+    'dynamodb', 
+    endpoint_url="http://localhost:8000",
+    region_name='us-west-2')
+
+def create_table():
+    table = dynamodb.create_table(
+        TableName='users',
+        KeySchema=[
+            {
+                'AttributeName': 'filename',
+                'KeyType': 'HASH'
+            },
+            {
+                'AttributeName': 'process_complete_date',
+                'KeyType': 'RANGE'
+            }
+        ],
+        AttributeDefinitions=[
+            {
+                'AttributeName': 'filename',
+                'AttributeType': 'S'
+            },
+            {
+                'AttributeName': 'process_complete_date',
+                'AttributeType': 'N'
+            },
+            {
+                'AttributeName': 'process_complete_year_month_day',
+                'AttributeType': 'S'   
+            }
+        ],
+        ProvisionedThroughput={
+            'ReadCapacityUnits': 4000,
+            'WriteCapacityUnits': 4000
+        },
+        GlobalSecondaryIndexes=[
+            {
+                'IndexName': 'idxDate',
+                'KeySchema': [
+                    {
+                        'AttributeName': 'process_complete_year_month_day',
+                        'KeyType': 'HASH'
+                    },
+                    {
+                        'AttributeName': 'process_complete_date',
+                        'KeyType': 'RANGE'
+                    },
+                ],
+                'Projection': {
+                    'ProjectionType': 'INCLUDE',
+                    'NonKeyAttributes': [
+                        'AAA', 'BBB', 'CCC'
+                    ]
+                },
+                'ProvisionedThroughput': {
+                    'ReadCapacityUnits': 4000,
+                    'WriteCapacityUnits': 4000
+                }
+            },
+        ]
+    )
+
+    table.meta.client.get_waiter('table_exists').wait(TableName='users')
+    print(table.item_count)
+    return table
+
+def put_item(table, ts):
+    # secondTimestamp = math.floor(utctime.timestamp())
+    # yyyyMMdd = utctime.strftime("%Y-%m-%d")
+
+    yyyyMMdd = datetime.datetime.utcfromtimestamp(ts).strftime("%Y-%m-%d")
+    print(yyyyMMdd)
+
+    response = table.put_item(
+        Item={
+            'filename': 'AAA.txt',
+            'process_complete_date': ts,
+            'process_complete_year_month_day': yyyyMMdd,
+            'AAA': 12,
+            'BBB': 32,
+            'CCC': 12,
+        }
+    )
+    print(table.item_count)
+
+
+def query_great_than(table, tsStart):
+    today = date.today()
+    startDate = datetime.datetime.fromtimestamp(tsStart, timezone.utc).date()
+    
+    allData = []
+    for single_date in daterange(startDate, today):
+        yyyyMMdd = single_date.strftime("%Y-%m-%d")
+
+        response = table.query(
+            IndexName='idxDate',
+            KeyConditionExpression=Key('process_complete_year_month_day').eq(yyyyMMdd)
+        )
+            
+        items = response['Items']
+        allData.extend(items)
+
+    return allData
+
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days)):
+        yield start_date + timedelta(n)
+
+
+def test_datatime_string():
+    localtime = datetime.datetime.now()
+    print(localtime)
+    utctime = datetime.datetime.now(tz=timezone.utc)
+    print(utctime)
+    print(utctime.strftime("%Y-%m-%dT%H:%M:%SZ"))
+    print(utctime.strftime("%Y-%m-%d"))
+    print(utctime.timestamp())
+    print(math.floor(utctime.timestamp()))
+
+
+def get_random_dt(start, end):
+    sdt = datetime.datetime.strptime(start, "%Y-%m-%d").timestamp()
+    edt = datetime.datetime.strptime(end, "%Y-%m-%d").timestamp()
+
+    count = 30
+    data = [randint(sdt, edt) for i in range(0, count)]
+    return data
+
+
+if __name__ == '__main__':
+    table = create_table()
+    
+    data = get_random_dt("2020-11-01", "2020-12-31")
+    for ts in data:
+        put_item(table, ts)
+    
+    tsTarget = datetime.datetime.strptime("2020-12-01", "%Y-%m-%d").timestamp()
+    allData = query_great_than(table, tsTarget)
+    print(allData)
+
