4d24d322c80304ef7427e1929db9416f8c88104f
LeeHemingway <coherence0815@gmail.com>
2020-12-28 10:27:01 +0900

    add on demand mode examples

---
+++ cloud/dynamodb/localServer/aws-cli/insertion/on_demand.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+export AWS_ACCESS_KEY_ID='DUMMYIDEXAMPLE'
+export AWS_SECRET_ACCESS_KEY='DUMMYEXAMPLEKEY'
+
+aws dynamodb create-table \
+  --table-name Students \
+  --attribute-definitions \
+    AttributeName=Name,AttributeType=S \
+  --key-schema \
+    AttributeName=Name,KeyType=HASH \
+  --region us-west-2 \
+  --endpoint-url http://localhost:8000 \
+  --billing-mode PAY_PER_REQUEST
+
+aws dynamodb list-tables --endpoint-url http://localhost:8000 --region us-west-2
+
+aws dynamodb put-item --table-name Students --region us-west-2 \
+  --endpoint-url http://localhost:8000 \
+  --item '{"Name": {"S": "James"}, "Age": {"N": "26"}, "Point": {"N": "2.43"}}'
+
+aws dynamodb put-item --table-name Students --region us-west-2 \
+  --endpoint-url http://localhost:8000 \
+  --item '{"Name": {"S": "Kenny"}, "Company": {"S": "TrendMicro"}, "IsMe": {"BOOL": true} }'
+
+aws dynamodb scan --table-name Students --region us-west-2 \
+  --endpoint-url http://localhost:8000
+
+
